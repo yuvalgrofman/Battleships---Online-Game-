a630ad6c66b3e077195a2ba04dc55f51d4f8370b
Yuval Grofman <yuvalgrofman@gmail.com>
2021-02-24 20:03:30 +0200

    Added A dialiog which gets from the user the theme

---
+++ src/game/ThemeDialog.java
@@ -0,0 +1,45 @@
+package game;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatCobalt2IJTheme;
+import com.formdev.flatlaf.intellijthemes.FlatGruvboxDarkMediumIJTheme;
+
+
+public class ThemeDialog {
+
+    public static final String[] pizzas = { "Flat Light", "Flat Dark", "Darcula", "Cobalt2", "Gruvbox Dark Medium" };
+    
+    public static void showPopUpGuiForTheme() {
+
+        String themePicked = (String) JOptionPane.showInputDialog(null, 
+        "Pick your theme?",
+        "Pick Theme",
+        JOptionPane.QUESTION_MESSAGE, 
+        null, 
+        pizzas, 
+        pizzas[0]);
+
+        if (themePicked.equals("Flat Light")) {
+            
+            FlatLightLaf.install();
+        }else if (themePicked.equals("Flat Dark")){
+
+            FlatDarkLaf.install();
+        } else if (themePicked.equals("Darcula")) {
+            
+            FlatDarculaLaf.install();
+        } else if (themePicked.equals("Cobalt2")) {
+
+            FlatCobalt2IJTheme.install();
+        } else {
+            
+            FlatGruvboxDarkMediumIJTheme.install();
+        }
+
+    }
+}
